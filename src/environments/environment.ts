1114f2b825b217fab6b63114176b79526c0f8c3c
Kevin Wagner <kevin_wagner4493@web.de>
2022-12-28 22:49:18 +0100

    first interface created and user list added

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDn-iERjBc-AJNwTyHjGSdvUu7X3CYSvoU",
+    authDomain: "simple-crm-d5c23.firebaseapp.com",
+    projectId: "simple-crm-d5c23",
+    storageBucket: "simple-crm-d5c23.appspot.com",
+    messagingSenderId: "171149111574",
+    appId: "1:171149111574:web:ff74cb33643b3a872de037"
+  },
+    production: false,
+
+};
