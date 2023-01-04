ae138258bdd9bdd4b2c5e39c5c343ff0dd337627
Kevin Wagner <kevin_wagner4493@web.de>
2023-01-04 20:41:37 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'user', component: UserComponent},
-  {path: 'user/:id', component: UserDetailComponent}
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: 'dashboard', 
+        component: DashboardComponent,
+        outlet: 'dashboard'
+      },
+      {
+        path: 'user', 
+        component: UserComponent,
+        outlet: 'user'
+      },
+      {
+        path: 'user/:id', 
+        component: UserDetailComponent,
+        outlet: 'user/id'
+      }
+    ]
+  },
 ];
 
 @NgModule({
